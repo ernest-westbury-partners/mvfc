53dc7317871f2943fa523f8933efe115ff9ee5b5
Ernest Epistola <ernestepistola@gmail.com>
2026-02-04 11:30:01 +0800

    Additional styles and initial commit of github workflow script.

--- grading/script.js
+++ grading/script.js
@@ -1,3 +1,21 @@
-$(document).ready(function () { 
-    console.log('ALSKJDSLKAJDHLSD Hello')
+$(document).ready(function () {
+    initTableSelect();
 })
+
+function initTableSelect() {
+    $('table').hide()
+    $('.table-select').each(function () {
+        $(this).find('h3').click(function () {
+            var target = $(this).data('target')
+            var title = $(this).text()
+            $('h3').removeClass('active')
+            $(this).addClass('active')
+            $('table').hide()
+            $(`#${target}`).show()
+
+            $('#title').text(title)
+        })
+    })
+
+    $('h3:first-child').click()
+}
